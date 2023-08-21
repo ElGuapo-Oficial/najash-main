641d6671cc816cbf97c0f98fc0c61383668d46b1
Najash Marron <najashmarron@gmail.com>
2023-08-20 18:32:14 -0700

    Adding cors to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,18 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema/schema';
 import resolvers from './resolvers/resolvers';
 import CitiesAPI from './datasources/cities-api';
+import cors from 'cors';  // Import cors
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     dataSources: () => ({
         CitiesAPI: new CitiesAPI()
-    })
+    }),
+    cors: {
+        origin: '*', // This allows all origins. You might want to restrict it in a production setting.
+        credentials: true
+    }
 })
 
 server.listen().then(() => {
